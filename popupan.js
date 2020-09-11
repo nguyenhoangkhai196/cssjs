00a89b339b891be5add263ba7c21418140653dbe
nguyenhoangkhai196 <nguyenhoangkhai196@gmail.com>
2020-09-11 20:24:56 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://goo.gl/4o6T4b';
+	var url_popup = 'https://otohondabinhtan.com/gia-xe-honda-crv-tai-phuoc-thanh/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
